f98b7984ac9ccad5f6c3bb6f470b6348373d6cdc
Yen Jung Chen <yenjungchen@Yens-MacBook-Pro-2.local>
2024-02-28 14:29:11 +0800

    add proxy logic

--- _worker.js
+++ _worker.js
@@ -8,28 +8,83 @@ export default {
         const token = generateSessionKey(50)
 
         // Set the session cookie
-        const cookie = `session=${sess}`;
+        const cookie = `session=${sess}; Path=/`;
         const headers = new Headers({ "Set-Cookie": cookie });
 
-        const store = env.SESSION_STORE;
-        await store.put(`sid:${sess}`, token, { expirationTtl: 60 });
+        const [ok, setkvtime] = await measureExecutionTime(async () => {
+          return await setTokenBySession(env, sess, token, 60)
+        })
 
-        return new Response(`Session cookie set ${sess}`, {
+        let message;
+        if (ok) {
+          message = `Session cookie set! Token: ${token}`
+        } else {
+          message = 'Failed to set cookie token in kv store'
+        }
+
+        return Response.json({
+          message: message,
+          meta: {
+            "x-set-kv-duration": setkvtime + "ms"
+          },
+          response_headers: Object.fromEntries(headers)
+        }, {
           status: 200,
           headers,
         });
       }  else if (pathname === "/queue") {
-        const queue = env.BG_QUEUE;
-        await queue.send({ message: "Hello!" })
+        const [ok, queuetime] = await measureExecutionTime(async () => {
+          return await queuePush(env, { message: "Hello!" })
+        })
+        let message = ok ? "true" : "false"
+        return Response.json({ push: message, meta: { "x-queue-duration": queuetime + "ms" } })
+      }  else if (pathname === "/demo-api") {
+        const originHeaders = Object.fromEntries(request.headers)
+        return Response.json({
+          message: "Proxy Success",
+          headers: originHeaders,
+          url: request.url,
+          body: request.body
+        }, { status: 200 }
+        )
       } else {
         const cookie = request.headers.get("Cookie");
         const sess = extractSessionCookie(cookie);
 
-        const store = env.SESSION_STORE;
-        const token = await store.get(`sid:${sess}`);
+        const [token, getkvtime] = await measureExecutionTime(async () => {
+          return await getTokenBySession(env, sess)
+        })
 
-        return Response.json({ current_session: sess, token: token }, { status: 200 })
+
+        if (token === null) {
+          return Response.json({
+            error: "Unauthorized.",
+            meta: {
+              'x-get-kv-duration': getkvtime + "ms"
+            }
+          }, { status: 401 })
+        }
+
+        const url = new URL(request.url);
+        const searchParams = new URLSearchParams(url.search);
+        const proxyUrl = `${url.origin}/demo-api` + searchParams.toString();
+
+        let headers = new Headers(request.headers);
+        headers.set('authorization', `Bearer ${token}`)
+        headers.set('x-cdc-web-proxy-hit', "1")
+
+        const options = {
+          method: request.method,
+          headers: headers,
+          body: request.method === 'GET' ? null : request.body,
+          redirect: 'manual'
+        };
+
+        const response = await fetch(proxyUrl, options);
+        return response
       }
+
+      return Response.json({ message: "success" })
     } catch(error) {
       console.log(error)
       // const queue = env.BG_QUEUE;
@@ -39,6 +94,37 @@ export default {
   },
 }
 
+
+async function queuePush(env, message) {
+  try {
+    const queue = env.BG_QUEUE;
+    await queue.send(message)
+    return true
+  } catch(error) {
+    console.log(error)
+    return false
+  }
+}
+
+async function setTokenBySession(env, session, token, ttl) {
+  try {
+    const store = env.SESSION_STORE;
+    await store.put(`sid:${session}`, token, { expirationTtl: ttl });
+    return true
+  } catch(error) {
+    console.log(error)
+    return false
+  }
+}
+async function getTokenBySession(env, session) {
+  try {
+    const store = env.SESSION_STORE;
+    return await store.get(`sid:${session}`);
+  } catch(error) {
+    console.log(error)
+    return null
+  }
+}
 
 function generateSessionKey(length) {
   const characters =
@@ -61,3 +147,13 @@ function extractSessionCookie(cookieHeader) {
   );
   return sessionCookie ? sessionCookie.split("=")[1] : "No session cookie found";
 }
+
+async function measureExecutionTime(fn) {
+  const startTime = Date.now(); // Get current timestamp before executing code
+  return fn().then(result => {
+    const endTime = Date.now(); // Get current timestamp after executing code
+    const executionTime = endTime - startTime; // Calculate elapsed time in milliseconds
+    return [result, executionTime];
+  });
+}
+
